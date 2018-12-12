f454235f51ac62a0c72aae4ea8a5788ae687ed7f
liuyang <454979849@qq.com>
2018-12-12 23:28:56 +0800

    node服务器端口号更改

--- server/index.js
+++ server/index.js
@@ -8,11 +8,11 @@ const app = new Koa();
 
 const server = require("http").createServer(app.callback());
 const io = require("socket.io")(server);
-server.listen(3000);
+server.listen(3389);
 app.use(cors());
 
 app.use(bodyParser());//获取post请求的参数的中间件
 app.use(router.routes()).use(router.allowedMethods())
 
 
-console.log("服务器已启动,端口3000");
+console.log("服务器已启动,端口3389");
