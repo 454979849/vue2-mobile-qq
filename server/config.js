ed1fcf98581421897473a9c7438264d1be96c9af
liuyang <liuyang@whzt.com>
2018-12-17 13:47:17 +0800

    开发进行中

--- server/config.js
+++ server/config.js
@@ -1,9 +1,10 @@
+//此处隐藏真实的私人数据库信息
 const db = {
-    host: '120.79.192.193', // 数据库IP
+    host: '127.0.0.1', // 数据库IP
     port: 3306, // 数据库端口
     database: 'qq', // 数据库名称
-    user: 'imooc', // 数据库用户名
-    password: 'sdfLKJ`12', // 数据库密码
+    user: 'root', // 数据库用户名
+    password: '123456', // 数据库密码
 }
 const baseApi = 'api/qq'
 
