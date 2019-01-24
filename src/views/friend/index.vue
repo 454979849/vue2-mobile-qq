20338e6b3e160557a504de9582ef7884eadbfd59
liuyang <liuyang@whzt.com>
2019-01-24 15:55:56 +0800

    开发进行中

--- src/views/friend/index.vue
+++ src/views/friend/index.vue
@@ -54,22 +54,50 @@
         name: "friend",
         components: {mHead, FriendList, GroupList, PublicList},
         //进入路由前执行的方法，因为该组件被缓存，于是生命周期不会重走，于是在这里设置footer的高亮
-        beforeRouteEnter(to,from,next) {
-            next(vm=>{
+        beforeRouteEnter(to, from, next) {
+            next(vm => {
                 vm.setFootIndex();
             });
         },
-        computed:{
+        computed: {
             ...mapGetters(['userInfo'])
         },
         created() {
             this.$store.commit('SHOW_FOOT_CHANGE', true);
-            getFriend(this.userInfo.id).then(res=>{
-                console.log(res);
+            getFriend(this.userInfo.id).then(res => {
+                res = res.data.friendList;
+                let friendList = [], groupNames = [];
+
+                res.forEach(item => {
+                    if (groupNames.indexOf(item.inA) == -1) {
+                        groupNames.push(item.inA);
+                        friendList.push({
+                            groupName: item.inA,
+                            subList: [{
+                                imgSrc: item.userHead,
+                                nickName: item.nickName,
+                                autograph: item.say,
+                                vip: item.isVip
+                            }]
+                        })
+                    } else {
+                        friendList.forEach((v,i) => {
+                            if (v.groupName == item.inA) {
+                                friendList[i].subList.push({
+                                    imgSrc: item.userHead,
+                                    nickName: item.nickName,
+                                    autograph: item.say,
+                                    vip: item.isVip
+                                })
+                            }
+                        })
+                    }
+                })
+                this.friendList=friendList;
             })
         },
         methods: {
-            setFootIndex(){
+            setFootIndex() {
                 this.$store.commit('SET_FOOT_INDEX', 1);
             },
             loadTop() {
@@ -91,119 +119,7 @@
         data() {
             return {
                 selected: 'tab1',
-                friendList: [{
-                    groupName: '分组一',
-                    subList: [{
-                        imgSrc: 'http://120.79.192.193/assets/headImgs/3.jpg',
-                        nickName: '狗子',
-                        autograph: '狗子的个性签名',
-                        vip: 2
-                    }, {
-                        imgSrc: 'http://120.79.192.193/assets/headImgs/4.jpg',
-                        nickName: '猫子',
-                        autograph: '猫子的个性签名',
-                        vip: 1
-                    }]
-                }, {
-                    groupName: '分组二',
-                    subList: [{
-                        imgSrc: 'http://120.79.192.193/assets/headImgs/5.jpg',
-                        nickName: '兔子',
-                        autograph: '兔子的个性签名',
-                        vip: 2
-                    }, {
-                        imgSrc: 'http://120.79.192.193/assets/headImgs/6.jpg',
-                        nickName: '鸡子',
-                        autograph: '鸡子的个性签名',
-                        vip: 1
-                    }, {
-                        imgSrc: 'http://120.79.192.193/assets/headImgs/7.jpg',
-                        nickName: '鸭子',
-                        autograph: '鸭子的个性签名',
-                        vip: 0
-                    }, {
-                        imgSrc: 'http://120.79.192.193/assets/headImgs/8.jpg',
-                        nickName: '败子',
-                        autograph: '败子的个性签名',
-                        vip: 0
-                    }, {
-                        imgSrc: 'http://120.79.192.193/assets/headImgs/9.jpg',
-                        nickName: '傻子',
-                        autograph: '傻子的个性签名',
-                        vip: 2
-                    }]
-                }, {
-                    groupName: '分组三',
-                    subList: [{
-                        imgSrc: 'http://120.79.192.193/assets/headImgs/10.jpg',
-                        nickName: '哈子',
-                        autograph: '哈子的个性签名',
-                        vip: 0
-                    }, {
-                        imgSrc: 'http://120.79.192.193/assets/headImgs/11.jpg',
-                        nickName: '猪子',
-                        autograph: '猪子的个性签名',
-                        vip: 0
-                    }, {
-                        imgSrc: 'http://120.79.192.193/assets/headImgs/12.jpg',
-                        nickName: '牛子',
-                        autograph: '牛子的个性签名',
-                        vip: 1
-                    }, {
-                        imgSrc: 'http://120.79.192.193/assets/headImgs/13.jpg',
-                        nickName: '羊子',
-                        autograph: '羊子的个性签名',
-                        vip: 0
-                    }, {
-                        imgSrc: 'http://120.79.192.193/assets/headImgs/14.jpg',
-                        nickName: '狮子',
-                        autograph: '狮子的个性签名',
-                        vip: 2
-                    }, {
-                        imgSrc: 'http://120.79.192.193/assets/headImgs/15.jpg',
-                        nickName: '虎子',
-                        autograph: '虎子的个性签名',
-                        vip: 0
-                    }, {
-                        imgSrc: 'http://120.79.192.193/assets/headImgs/16.jpg',
-                        nickName: '狼子',
-                        autograph: '狼子的个性签名',
-                        vip: 0
-                    }, {
-                        imgSrc: 'http://120.79.192.193/assets/headImgs/17.jpg',
-                        nickName: '鸟子',
-                        autograph: '鸟子的个性签名',
-                        vip: 1
-                    }, {
-                        imgSrc: 'http://120.79.192.193/assets/headImgs/18.jpg',
-                        nickName: '蛇子',
-                        autograph: '蛇子的个性签名',
-                        vip: 1
-                    }]
-                }, {
-                    groupName: '分组四',
-                    subList: [{
-                        imgSrc: 'http://120.79.192.193/assets/headImgs/19.jpg',
-                        nickName: '疯子',
-                        autograph: '疯子的个性签名',
-                        vip: 0
-                    }, {
-                        imgSrc: 'http://120.79.192.193/assets/headImgs/20.jpg',
-                        nickName: '野子',
-                        autograph: '野子的个性签名',
-                        vip: 0
-                    }, {
-                        imgSrc: 'http://120.79.192.193/assets/headImgs/21.jpg',
-                        nickName: '鸽子',
-                        autograph: '鸽子的个性签名',
-                        vip: 2
-                    }, {
-                        imgSrc: 'http://120.79.192.193/assets/headImgs/22.jpg',
-                        nickName: '豹子',
-                        autograph: '豹子的个性签名',
-                        vip: 0
-                    }]
-                }],
+                friendList:[],
                 publicList: [{
                     letter: 'D',
                     subList: [{
@@ -255,11 +171,13 @@
         background-color: #f7f7f9;
         overflow-y: auto;
         -webkit-overflow-scrolling: touch;
+
         .mint-tab-container-item {
             background-color: #fff;
             margin-top: .03rem;
             margin-bottom: .5rem;
         }
+
         .info {
             border: .01rem solid #eee;
             height: .9rem;
@@ -269,10 +187,12 @@
             padding-left: .1rem;
             display: flex;
             align-items: center;
+
             span {
                 color: #000;
                 font-size: .15rem;
             }
+
             p {
                 font-size: .1rem;
                 color: #666;
